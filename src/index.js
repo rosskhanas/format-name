f9c18c8bd15034e8889802091395e215a52634df
Rostyslav Khanas <rtkhanas@gmail.com>
2016-12-17 11:58:47 +0900

    Changed string values to int

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
 const CJK_REGEX = /[⺀-\u2efe\u3000-〾\u3040-ゞ゠-ヾ㇀-\u31eeㇰ-ㇾ㈀-㋾㌀-㏾㐀-\u4dbe一-\u9ffe豈-\ufafe︰-﹎]|[\ud840-\ud868\ud86a-\ud86c][\udc00-\udfff]|\ud82c[\udc00-\udcfe]|\ud869[\udc00-\udede\udf00-\udfff]|\ud86d[\udc00-\udf3e\udf40-\udfff]|\ud86e[\udc00-\udc1e]|\ud87e[\udc00-\ude1e]/;
 /* eslint-enable */
 
-export const FIRST_LAST = 'FIRST_LAST';
-export const LAST_FIRST = 'LAST_FIRST';
+export const FIRST_LAST = 1;
+export const LAST_FIRST = 2;
 
-const isValidString = function (val) {
+function isValidString(val) {
   return (typeof val === 'string') && val.length;
-};
+}
 
 export default (firstName, lastName, sortOrder = FIRST_LAST) => {
   // if some of values are not defined
