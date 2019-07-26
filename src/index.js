bb27fd1d53620c274f515c52e6145ccc0f5b8471
Ross Khanas <10609041+rosskhanas@users.noreply.github.com>
2019-07-26 12:39:19 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import trim from 'lodash/trim';
 
 /* eslint-disable quotes, max-len */
 const CJK_REGEX = /[⺀-\u2efe\u3000-〾\u3040-ゞ゠-ヾ㇀-\u31eeㇰ-ㇾ㈀-㋾㌀-㏾㐀-\u4dbe一-\u9ffe豈-\ufafe︰-﹎]|[\ud840-\ud868\ud86a-\ud86c][\udc00-\udfff]|\ud82c[\udc00-\udcfe]|\ud869[\udc00-\udede\udf00-\udfff]|\ud86d[\udc00-\udf3e\udf40-\udfff]|\ud86e[\udc00-\udc1e]|\ud87e[\udc00-\ude1e]/;
-/* eslint-enable */
+/* eslint-enable quotes, max-len */
 
 export const FIRST_LAST = 1;
 export const LAST_FIRST = 2;
