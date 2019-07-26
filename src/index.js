884f3670621854abbe0c226d755a5820a5b274a0
Rostyslav Khanas <rtkhanas@gmail.com>
2019-07-26 12:48:40 -0400

    refactor: get rid of lodash

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import trim from 'lodash/trim';
-
 /* eslint-disable quotes, max-len */
 const CJK_REGEX = /[⺀-\u2efe\u3000-〾\u3040-ゞ゠-ヾ㇀-\u31eeㇰ-ㇾ㈀-㋾㌀-㏾㐀-\u4dbe一-\u9ffe豈-\ufafe︰-﹎]|[\ud840-\ud868\ud86a-\ud86c][\udc00-\udfff]|\ud82c[\udc00-\udcfe]|\ud869[\udc00-\udede\udf00-\udfff]|\ud86d[\udc00-\udf3e\udf40-\udfff]|\ud86e[\udc00-\udc1e]|\ud87e[\udc00-\ude1e]/;
 /* eslint-enable quotes, max-len */
@@ -17,8 +15,8 @@ export function isCJKFullName(name, surname) {
   if (!isNameValid || !isSurnameValid) {
     return false;
   }
-  const nameTrimmed = trim(name);
-  const surnameTrimmed = trim(surname);
+  const nameTrimmed = isStringValid(name) ? name.trim() : '';
+  const surnameTrimmed = isStringValid(surname) ? surname.trim() : '';
   const endOfNameIsCJK = CJK_REGEX.test(nameTrimmed[nameTrimmed.length - 1]);
   const beginOfSurnameIsCJK = CJK_REGEX.test(surnameTrimmed[0]);
   if (beginOfSurnameIsCJK || endOfNameIsCJK) {
@@ -28,8 +26,8 @@ export function isCJKFullName(name, surname) {
 }
 
 export default (name, surname, sortOrder = FIRST_LAST) => {
-  const nameTrimmed = trim(name);
-  const surnameTrimmed = trim(surname);
+  const nameTrimmed = isStringValid(name) ? name.trim() : '';
+  const surnameTrimmed = isStringValid(surname) ? surname.trim() : '';
   // Some of names are not defined.
   const isNameValid = isStringValid(name);
   const isSurnameValid = isStringValid(surname);
